41fb23fa82e8612feab859d27ff7d9421b1a6b8f
Ahmad Hesam <hesam212@gmail.com>
2018-08-13 17:41:18 +0200

    Add benchmark for initial (isolated) GPU code + sorting improvement

--- Makefile
+++ Makefile
@@ -2,10 +2,10 @@
 all: physics
 
 libcudaphysics.so: cuda.cu cuda.h
-	@nvcc -std=c++11 -arch=sm_50 --shared -g --compiler-options -fPIC  cuda.cu -o libcudaphysics.so
+	@nvcc -std=c++11 -arch=sm_50 --shared --compiler-options -fPIC  cuda.cu -o libcudaphysics.so
 
 physics: physics.cpp cpu.h gpu.h libcudaphysics.so helper.h
-	@g++ -L/usr/local/cuda-8.0/lib64 -std=c++11 -g -O3 -I. -I/usr/local/cuda-8.0/include physics.cpp -o physics -fopenmp -lOpenCL libcudaphysics.so
+	@g++ -L/usr/local/cuda-8.0/lib64 -std=c++11 -O3 -I. -I/usr/local/cuda-8.0/include physics.cpp -o physics -fopenmp -lOpenCL libcudaphysics.so
 
 clean:
 	rm physics
--- gpu.sh
+++ gpu.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 # for each hardware type (CPU, OpenCL, CUDA)
-for h in 0
+for h in 0 2
 do
 	echo "--------------------------------------- ${h} HARDWARE"
 	echo --------------------------------------- REGULAR ACCESS
 	echo --------------------------------------- $n NEIGHBORS
-	for i in 64 96 128 150 180 200 220 256 275 300 320
+	
+        if [[ "$h" -eq 2 ]]; then
+          echo "Warming GPU up..."
+          echo `taskset -c 0-9,20-29 ./physics $h 128 10 20 1`
+        fi
+        
+        for i in 16 32 64 96 128 150 180 200
 	do
 		echo "$i number of cells"
-		echo `./physics $h $i 20 16 1` >> "regular_${h}h_16n_1t.txt"
+		echo `taskset -c 0-9,20-29 ./physics $h $i 10 38 20` >> "regular_${h}h_16n_20t.txt"
 	done
 
 	echo ""
@@ -17,10 +23,10 @@ do
 
 	echo --------------------------------------- RANDOM ACCESS
 	echo --------------------------------------- $n NEIGHBORS
-	for i in 64 96 128 150 180 200 220 256 275 300 320
+	for i in 16 32 64 96 128 150 180 200
 	do
 		echo "$i number of cells"
-		echo `./physics $h $i 20 16 1 r` >> "random_${h}h_16n_1t.txt"
+		echo `taskset -c 0-9,20-29 ./physics $h $i 10 38 20 r` >> "random_${h}h_16n_20t.txt"
 	done
 	echo ""
 done
